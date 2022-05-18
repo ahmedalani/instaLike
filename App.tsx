ff97b4823caa991fac56c53d294ce397cb34ea96
Ahmed Alani <s3leha@gmail.com>
2022-05-18 15:15:48 -0700

    double pressed feature for likes, new color added, couple componenets

--- App.tsx
+++ App.tsx
@@ -9,58 +9,23 @@
  */
 
 import React from 'react';
-import {StyleSheet, ScrollView} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
-import FeedPost from './src/components/FeedPost';
-
-import {IPost} from './src/types/models';
-const post: IPost = {
-  id: '1',
-  createdAt: '19 December 2021',
-  image:
-    'https://www.uptowneyecare.com/wp-content/uploads/2019/03/green-eyes.jpg',
-  description:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic repellendus unde blanditiis. Eos fugiat dolorem ea fugit aut sapiente corrupti autem dolores deleniti architecto, omnis, amet unde dignissimos quam minima?',
-  user: {
-    id: '1-jamieHots',
-    image:
-      'https://www.uptowneyecare.com/wp-content/uploads/2019/03/green-eyes.jpg',
-    username: 'JamieHots',
-  },
-  nofComments: 11,
-  nofLikes: 33,
-  comments: [
-    {
-      id: '1',
-      comment: 'Hello there',
-      user: {
-        id: '1-jamieHots',
-        username: 'jamieHots',
-      },
-    },
-    {
-      id: '2',
-      comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. H',
-      user: {
-        id: '2-markFox',
-        username: 'markFox',
-      },
-    },
-  ],
-};
+// screens
+import HomeScreen from './src/screens/HomeScreen';
 
 const App = () => {
   return (
-    <ScrollView style={styles.app}>
-      <FeedPost post={post} />
-    </ScrollView>
+    <View style={styles.app}>
+      <HomeScreen />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   app: {
     flex: 1,
-    marginTop: 25,
+    marginTop: 30,
   },
 });
 
--- src/components/FeedPost/FeedPost.tsx
+++ src/components/FeedPost/FeedPost.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Text, View, Image} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, Image, Pressable} from 'react-native';
 
 // vector icons
 import Entypo from 'react-native-vector-icons/Entypo';
@@ -12,12 +12,27 @@ import colors from '../../theme/colors';
 
 // components
 import Comment from '../Comment';
+import DoublePressable from '../DoublePressable';
 
 import {IPost} from '../../types/models';
 interface IFeedPost {
   post: IPost;
 }
 const FeedPost = ({post}: IFeedPost) => {
+  // states:
+  const [isDescriptionExpanded, setIsDescriptionExpanded] = useState<Boolean>(false);
+  const [isLiked, setIsLiked] = useState<Boolean>(false);
+
+  // helper Functions:
+  // expand or contrast the post description
+  const toggleDescriptionExpanded = () => {
+    setIsDescriptionExpanded(currV => !currV);
+  };
+  // set or unset the like for the post
+  const toggleLike = () => {
+    setIsLiked(currV => !currV);
+  };
+
   return (
     <View style={styles.post}>
       {/* Header */}
@@ -29,61 +44,38 @@ const FeedPost = ({post}: IFeedPost) => {
           style={styles.userAvatar}
         />
         <Text style={styles.userName}>{post.user.username}</Text>
-        <Entypo
-          name="dots-three-horizontal"
-          size={16}
-          style={styles.threeDots}
-        />
+        <Entypo name="dots-three-horizontal" size={16} style={styles.threeDots} />
       </View>
       {/* Content */}
-      <Image
-        source={{
-          uri: post.image,
-        }}
-        style={styles.image}
-      />
+      {/* doublePressable is custom componenet to handle double touch a picture and toggle the like function */}
+      <DoublePressable onDoublePress={toggleLike}>
+        <Image
+          source={{
+            uri: post.image,
+          }}
+          style={styles.image}
+        />
+      </DoublePressable>
       {/* Footer */}
       <View style={styles.footer}>
         <View style={styles.iconContainer}>
-          <AntDesign
-            name={'hearto'}
-            size={24}
-            style={styles.icon}
-            color={colors.black}
-          />
-          <Ionicons
-            name="chatbubble-outline"
-            size={24}
-            style={styles.icon}
-            color={colors.black}
-          />
-          <Feather
-            name="send"
-            size={24}
-            style={styles.icon}
-            color={colors.black}
-          />
-          <Feather
-            name="bookmark"
-            size={24}
-            style={{marginLeft: 'auto'}}
-            color={colors.black}
-          />
+          <Pressable onPress={toggleLike}>
+            <AntDesign name={isLiked ? 'heart' : 'hearto'} size={24} style={styles.icon} color={isLiked ? colors.accent : colors.black} />
+          </Pressable>
+          <Ionicons name="chatbubble-outline" size={24} style={styles.icon} color={colors.black} />
+          <Feather name="send" size={24} style={styles.icon} color={colors.black} />
+          <Feather name="bookmark" size={24} style={{marginLeft: 'auto'}} color={colors.black} />
         </View>
         {/* Likes */}
         <Text style={styles.text}>
-          Liked by <Text style={styles.bold}>someUserName</Text> and{' '}
-          <Text style={styles.bold}>{post.nofLikes} others</Text>
+          Liked by <Text style={styles.bold}>someUserName</Text> and <Text style={styles.bold}>{post.nofLikes} others</Text>
         </Text>
         {/* post description */}
-        <Text style={styles.text}>
-          <Text style={styles.bold}>{post.user.username}</Text>{' '}
-          {post.description}
+        <Text style={styles.text} onPress={toggleDescriptionExpanded} numberOfLines={isDescriptionExpanded ? 0 : 2}>
+          <Text style={styles.bold}>{post.user.username}</Text> {post.description}
         </Text>
         {/* Comments */}
-        <Text style={{color: colors.grey}}>
-          View all {post.nofComments} comments
-        </Text>
+        <Text style={{color: colors.grey}}>View all {post.nofComments} comments</Text>
         {post.comments.map(c => {
           return <Comment key={c.id} comment={c} />;
         })}
