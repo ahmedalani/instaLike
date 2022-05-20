f35094a3fb106d5b23713b9c4af65c70725c750e
Ahmed Alani <s3leha@gmail.com>
2022-05-20 14:07:07 -0700

    updated video player to show duration time

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, Pressable} from 'react-native';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
 import Video from 'react-native-video';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
@@ -11,9 +11,21 @@ interface IVideoPlayer {
 }
 const VideoPlayer = ({uri, paused}: IVideoPlayer) => {
   const [muted, setMuted] = useState(true);
+  const [durationInSec, setDurationInSec] = useState(0);
   return (
     <View>
-      <Video source={{uri}} style={styles.video} resizeMode="cover" repeat={true} muted={muted} paused={paused} />
+      <Video
+        source={{uri}}
+        style={styles.video}
+        resizeMode="cover"
+        repeat={true}
+        muted={muted}
+        paused={paused}
+        onProgress={e => setDurationInSec(Math.round(e.seekableDuration - e.currentTime))}
+      />
+      <View style={styles.duration}>
+        <Text>{durationInSec}</Text>
+      </View>
       <Pressable onPress={() => setMuted(currV => !currV)} style={styles.muteButton}>
         <Ionicons name={muted ? 'volume-mute' : 'volume-medium'} size={14} color={colors.white} />
       </Pressable>
@@ -26,8 +38,16 @@ const styles = StyleSheet.create({
     width: '100%',
     aspectRatio: 1,
   },
+  duration: {
+    backgroundColor: colors.white,
+    padding: 5,
+    borderRadius: 25,
+    position: 'absolute',
+    top: 10,
+    right: 10,
+  },
   muteButton: {
-    backgroundColor: colors.black,
+    backgroundColor: colors.grey,
     padding: 5,
     borderRadius: 25,
     position: 'absolute',
