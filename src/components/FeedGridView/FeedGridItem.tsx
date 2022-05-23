67e5b8d0ae6cd673d193bff611a693075c15841e
Ahmed Alani <s3leha@gmail.com>
2022-05-23 15:55:19 -0700

    type checking

--- src/components/FeedGridView/FeedGridItem.tsx
+++ src/components/FeedGridView/FeedGridItem.tsx
@@ -7,7 +7,7 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 const FeedGridItem = ({post}: {post: IPost}) => {
   return (
     <View style={{flex: 1, aspectRatio: 1, padding: 1, maxWidth: '33.33%'}}>
-      <Image source={{uri: post.image || post.images[0]}} style={{flex: 1}} />
+      <Image source={{uri: post.images ? post.images[0] : post.image}} style={{flex: 1}} />
       {post.images && <MaterialIcons name="collections" size={16} color={colors.white} style={{position: 'absolute', top: 5, right: 5}} />}
     </View>
   );
