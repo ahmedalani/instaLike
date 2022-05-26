0f359f54e0128243154a78000d6e32baf58835de
Ahmed Alani <s3leha@gmail.com>
2022-05-26 14:27:42 -0700

    ran the app on physical iphone

--- src/screens/PostUploadScreen/PostUploadScreen.tsx
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -1,12 +1,67 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {Camera} from 'expo-camera';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import colors from '../../theme/colors';
 
 const PostUploadScreen = () => {
+  const [hasPermissions, setHasPermissions] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const getPermission = async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const microphonPermission = await Camera.requestMicrophonePermissionsAsync();
+      setHasPermissions(cameraPermission.status === 'granted' && microphonPermission.status === 'granted');
+    };
+    getPermission();
+  }, []);
+
+  if (hasPermissions === null) {
+    return <Text>Loading...</Text>;
+  }
+  if (hasPermissions === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View>
-      <Text>PostUploadScreen</Text>
+    <View style={styles.page}>
+      <Camera style={styles.camera} />
+      <View style={[styles.buttonContainer, {top: 25}]}>
+        <MaterialIcons name="close" size={30} color={colors.white} />
+        <MaterialIcons name="flash-off" size={30} color={colors.white} />
+        <MaterialIcons name="settings" size={30} color={colors.white} />
+      </View>
+      <View style={[styles.buttonContainer, {bottom: 25}]}>
+        <MaterialIcons name="photo-library" size={30} color={colors.white} />
+        <View style={styles.circle} />
+        <MaterialIcons name="flip-camera-ios" size={30} color={colors.white} />
+      </View>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: colors.black,
+  },
+  camera: {
+    width: '100%',
+    aspectRatio: 3 / 4,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+  },
+  circle: {
+    width: 75,
+    aspectRatio: 1,
+    borderRadius: 75,
+    backgroundColor: colors.white,
+  },
+});
 export default PostUploadScreen;
