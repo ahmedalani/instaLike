a8d829688aec67a86289759722b6dd3f5525ebac
Ahmed Alani <s3leha@gmail.com>
2022-06-16 14:06:20 -0700

    deeplinks to user profile and other links implemented

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {NavigationContainer} from '@react-navigation/native';
+import {LinkingOptions, NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootNavigatorParamList} from './types';
 
@@ -12,9 +12,30 @@ import CommentsScreen from '../screens/CommentsScreen';
 
 const Stack = createNativeStackNavigator<RootNavigatorParamList>();
 
+// deeplinking configs
+const linking: LinkingOptions<RootNavigatorParamList> = {
+  prefixes: ['instalike://', 'https://instalike.com'],
+  config: {
+    initialRouteName: 'Home',
+    screens: {
+      Comments: 'comments',
+      // instalike://user/id
+      Home: {
+        screens: {
+          HomeStack: {
+            initialRouteName: 'Feed',
+            screens: {
+              UserProfile: 'user/:userId',
+            },
+          },
+        },
+      },
+    },
+  },
+};
 const Navigation = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={BottomTabNavigator} />
         {/* <Stack.Screen name="Feed" component={HomeScreen} options={{headerTitle: HeaderTitle, headerTitleAlign: 'center'}} /> */}
